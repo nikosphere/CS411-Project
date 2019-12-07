2516d369da86a0471b33a53e04888029e1a0cc86
Nikolay Valov <valov.nikolay@gmail.com>
2019-12-07 10:50:47 -0500

    codeCleanUp

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\valov\PycharmProjects\CS411_Project_V1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
